0eac884226f3db9484baf3cf23bea7344f6ac329
Harry Archer Blackburn <harryblackburn@harrys-mbp.home>
2017-10-11 10:38:27 +0100

    css refactoring

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
 document.getElementById("form1").onsubmit=function() {
-    variable = parseInt(document.querySelector('input[name = "variable"]:checked').value);
-    sub = parseInt(document.querySelector('input[name = "sub"]:checked').value);
-    con = parseInt(document.querySelector('input[name = "con"]:checked').value);
-    ifstate = parseInt(document.querySelector('input[name = "ifstate"]:checked').value);
+    variable = parseInt(document.querySelector('input[name = "football"]:checked').value);
+    sub = parseInt(document.querySelector('input[name = "city"]:checked').value);
+    con = parseInt(document.querySelector('input[name = "music"]:checked').value);
+    ifstate = parseInt(document.querySelector('input[name = "history"]:checked').value);
     
     
     result = variable + sub + con + ifstate;
     
  document.getElementById("grade").innerHTML = result;
-    
 
-
-return false; // required to not refresh the page; just leave this here
-} //this ends the submit function
-
+ if (result == 0) {result2 = "I dont think you belong here :D"};
+ if (result == 25) {result2 = "You need to spend more time with the locals"};
+ if (result == 50) {result2 = "I think you could do better, spend more time in local pubs"};
+ if (result == 75) {result2 = "So close. Try again"};
+ if (result == 100) {result2 = "Excellent! You're an honorary manc"};
+ document.getElementById("grade2").innerHTML = result2;
+ 
+     return false;
+         }
